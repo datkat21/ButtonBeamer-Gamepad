443bd1ece9549e81cc36d48b9efcabbadae53f58
datkat21 <datkat21.yt@gmail.com>
2024-10-21 16:30:37 -0500

    update to work with latest node, attempt change of submodule location to newest fork

--- client/client.js
+++ client/client.js
@@ -1,44 +1,64 @@
-$(function() {
+window.addEventListener("load", async () => {
   var socket = io();
-  $('.message-form').submit(function(e) {
+  document
+    .querySelector(".message-form")
+    .addEventListener("submit", function (e) {
+      e.preventDefault();
+      socket.emit("chat message", document.querySelector("#m").value);
+      document.querySelector("#m").value = "";
+      return false;
+    });
+  document.querySelector("#input").addEventListener("keydown", function (e) {
     e.preventDefault();
-    socket.emit('chat message', $('#m').val());
-    $('#m').val('');
+    if (e.repeat) return;
+    socket.volatile.emit("input", {
+      type: "keydown",
+      code: e.code,
+      name: e.key,
+    });
     return false;
   });
-  $('#input').keydown(function(e) {
+  document.querySelector("#input").addEventListener("keyup", function (e) {
     e.preventDefault();
-    socket.volatile.emit('input', {type: 'keydown', code: e.code, name: e.key});
+    socket.volatile.emit("input", { type: "keyup", code: e.code, name: e.key });
     return false;
   });
-  $('#input').keyup(function(e) {
-    e.preventDefault();
-    socket.volatile.emit('input', {type: 'keyup', code: e.code, name: e.key});
-    return false;
-  });
-  
+
   function pushMessage(message, css) {
     if (!css) css = {};
-    $('#messages')
-      .append($('<li>').text(message).css(css))
-      .scrollTop(function() { return this.scrollHeight; });
+    const msgs = document.querySelector("#messages");
+    let li = document.createElement("li");
+    li.innerText = message;
+    for (const key in css) {
+      li.style.setProperty(key, css[key]);
+    }
+    msgs.appendChild(li);
+    msgs.scrollTop = msgs.scrollHeight;
   }
-  socket.on('connect', function() {
-    pushMessage("you've connected.", {color: 'green'});
+  socket.on("connect", function () {
+    pushMessage("you've connected.", { color: "green" });
   });
-  socket.on('disconnect', function(reason) {
-    pushMessage("📵 you've been disconnected: " + reason + ".", {color: 'red'});
+  socket.on("disconnect", function (reason) {
+    pushMessage("📵 you've been disconnected: " + reason + ".", {
+      color: "red",
+    });
   });
-  socket.on('user connected', function(username) {
-    pushMessage(username + ' connected.');
+  socket.on("user connected", function (username) {
+    pushMessage(username + " connected.");
   });
-  socket.on('user disconnected', function(username) {
-    pushMessage(username + ' disconnected.');
+  socket.on("user disconnected", function (username) {
+    pushMessage(username + " disconnected.");
   });
-  socket.on('chat message', function(chatMessage) {
-    pushMessage(chatMessage.username + ': ' + chatMessage.content);
+  socket.on("chat message", function (chatMessage) {
+    pushMessage(chatMessage.username + ": " + chatMessage.content);
   });
-  socket.on('nickname change', function(nicknameChange) {
-    pushMessage("user '" + nicknameChange.oldName + "' changed nickname to '" + nicknameChange.newName + "'.");
+  socket.on("nickname change", function (nicknameChange) {
+    pushMessage(
+      "user '" +
+        nicknameChange.oldName +
+        "' changed nickname to '" +
+        nicknameChange.newName +
+        "'."
+    );
   });
 });
--- index.js
+++ index.js
@@ -1,17 +1,19 @@
-const keycodeToMessage = require('./keycode_to_message');
+const keycodeToMessage = require("./keycode_to_message");
 
-const express = require('express');
+const express = require("express");
 const app = express();
-const http = require('http').createServer(app);
-const io = require('socket.io')(http);
-const path = require('path');
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
+const path = require("path");
 
-const outSocket = process.env.OUTPUT_SOCKET_PATH ? require('unix-dgram').createSocket('unix_dgram') : null;
+const outSocket = process.env.OUTPUT_SOCKET_PATH
+  ? require("unix-dgram").createSocket("unix_dgram")
+  : null;
 
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 const outSocketPath = process.env.OUTPUT_SOCKET_PATH;
 
-app.use('/', express.static(path.join(__dirname, 'client')));
+app.use("/", express.static(path.join(__dirname, "client")));
 
 var usernames = new Map();
 var userLastID = 1;
@@ -21,113 +23,129 @@ function relay(message) {
   else console.log(`[TEST RELAY]: ${message}`);
 }
 
-io.on('connection', (socket) => {
+io.on("connection", (socket) => {
   function sendSystemMessage(content) {
-    socket.emit('chat message', {username: 'system', content: content});
+    socket.emit("chat message", { username: "system", content: content });
   }
   const slashCommands = {
     help: {
-      description: 'list all commands',
-      run: function() {
-        let helpText = '\n';
+      description: "list all commands",
+      run: function () {
+        let helpText = "\n";
         for (command in slashCommands) {
-          const args = slashCommands[command]?.args?.map(arg => `«${arg}»`).join(' ');
-          helpText += `/${command} ${args ? args + ' ' : ''}– ${slashCommands[command].description}\n`
+          const args = slashCommands[command]?.args
+            ?.map((arg) => `«${arg}»`)
+            .join(" ");
+          helpText += `/${command} ${args ? args + " " : ""}– ${
+            slashCommands[command].description
+          }\n`;
         }
         sendSystemMessage(helpText);
-      }
+      },
     },
     nick: {
       rawArg: true,
-      args: ['new name'],
-      description: 'set your nickname',
-      run: function(userId, newUsername) { 
+      args: ["new name"],
+      description: "set your nickname",
+      run: function (userId, newUsername) {
         const oldUsername = usernames.get(userId);
         usernames.set(userId, newUsername);
-        io.emit('nickname change', {oldName: oldUsername, newName: newUsername});
-      }
+        io.emit("nickname change", {
+          oldName: oldUsername,
+          newName: newUsername,
+        });
+      },
     },
     emu: {
       rawArg: true,
-      args: ['command'],
-      description: 'send raw wmemulator command',
-      run: function(userId, command) {
+      args: ["command"],
+      description: "send raw wmemulator command",
+      run: function (userId, command) {
         relay(Buffer.from(command));
-      }
+      },
     },
     map: {
-      args: ['key', 'input type', 'input_name'],
-      description: '(re)map key to input',
-      run: function(userId, keycode, inputType, inputName) {
+      args: ["key", "input type", "input_name"],
+      description: "(re)map key to input",
+      run: function (userId, keycode, inputType, inputName) {
         keycodeToMessage.map(userId, keycode, [inputType, inputName]);
         sendSystemMessage(`Mapped ${keycode} to ${inputType} ${inputName}`);
-      }
+      },
     },
-    'map-list': {
-      description: 'list key-input mappings',
-      run: function(userId, keycode, inputType, inputName) {
-        let listing = 'Your current key-input mappings (format: key type name):\n';
+    "map-list": {
+      description: "list key-input mappings",
+      run: function (userId, keycode, inputType, inputName) {
+        let listing =
+          "Your current key-input mappings (format: key type name):\n";
         const profile = keycodeToMessage.profile(userId);
         for (keycode in profile) {
-          listing += `${keycode} ${profile[keycode].join(' ')}\n`;
+          listing += `${keycode} ${profile[keycode].join(" ")}\n`;
         }
         sendSystemMessage(listing);
-      }
-    }
+      },
+    },
   };
-  
+
   const defaultUsername = `user #${userLastID++}`;
   console.error(`user connected: ${defaultUsername}`);
   usernames.set(socket.id, defaultUsername);
-  io.emit('user connected', defaultUsername);
-  sendSystemMessage('use /help to list all commands')
-  
-  socket.on('disconnect', () => {
+  io.emit("user connected", defaultUsername);
+  sendSystemMessage("use /help to list all commands");
+
+  socket.on("disconnect", () => {
     const currentUsername = usernames.get(socket.id);
     console.error(`user disconnected: ${currentUsername}`);
-    io.emit('user disconnected', currentUsername);
+    io.emit("user disconnected", currentUsername);
   });
-  
-  socket.on('chat message', (message) => {
-    if (message.startsWith('/')) {
-      message = message.substring('/'.length).trim();
-      let spaceIndex = message.indexOf(' ');
-      const command = (spaceIndex === -1) ? message : message.substring(0, spaceIndex);
+
+  socket.on("chat message", (message) => {
+    if (message.startsWith("/")) {
+      message = message.substring("/".length).trim();
+      let spaceIndex = message.indexOf(" ");
+      const command =
+        spaceIndex === -1 ? message : message.substring(0, spaceIndex);
       if (!(command in slashCommands)) {
-        sendSystemMessage('no such command; use /help to list all commands');
+        sendSystemMessage("no such command; use /help to list all commands");
         return;
       }
       const commandInfo = slashCommands[command];
       message = message.substring(command.length).trim();
-      const args = commandInfo?.rawArg ? [message] : message.split(' ').filter(s => s !== '');
+      const args = commandInfo?.rawArg
+        ? [message]
+        : message.split(" ").filter((s) => s !== "");
       const expectedArgsCount = commandInfo?.args?.length ?? 0;
       if (args.length !== expectedArgsCount) {
-        sendSystemMessage(`expected ${expectedArgsCount} arguments but got ${args.length}`);
+        sendSystemMessage(
+          `expected ${expectedArgsCount} arguments but got ${args.length}`
+        );
         return;
       }
       commandInfo.run(socket.id, ...args);
       return;
     } else {
-      io.emit('chat message', {username: usernames.get(socket.id), content: message});
+      io.emit("chat message", {
+        username: usernames.get(socket.id),
+        content: message,
+      });
     }
   });
-  
-  socket.on('input', (input) => {
-		let code = input.code;
-		if (code.startsWith("Key")) code = code.substring(3);
-    
+
+  socket.on("input", (input) => {
+    let code = input.code;
+    if (code.startsWith("Key")) code = code.substring(3);
+
     const converted = keycodeToMessage.convert(socket.id, code);
     if (converted) {
       const [eventType, eventParam] = converted;
-      const eventStatus = (input.type === 'keydown') ? '1' : '0';
+      const eventStatus = input.type === "keydown" ? "1" : "0";
       const message = Buffer.from(`${eventType} ${eventStatus} ${eventParam}`);
       relay(message);
     } else {
       console.log(`No message corresponding to key code: ${code}`);
     }
   });
-  
-  relay(Buffer.from('hotplug 1 nunchuk'));
+
+  relay(Buffer.from("hotplug 1 nunchuk"));
 });
 
 http.listen(port, () => {
@@ -135,16 +153,17 @@ http.listen(port, () => {
 });
 
 if (outSocket) {
-  outSocket.on('error', (err) => {
+  outSocket.on("error", (err) => {
     console.error(err);
   });
-  outSocket.on('connect', (err) => {
+  outSocket.on("connect", (err) => {
     console.log(`Relaying to UNIX socket at ${outSocketPath}`);
   });
 
   console.log(`Attempting to connect to UNIX socket at ${outSocketPath}`);
   outSocket.connect(outSocketPath);
 } else {
-  console.log('TEST MODE (NOT RELAYING INPUT)\nTo relay input, set env var OUTPUT_SOCKET_PATH to a (nonempty) path value');
+  console.log(
+    "TEST MODE (NOT RELAYING INPUT)\nTo relay input, set env var OUTPUT_SOCKET_PATH to a (nonempty) path value"
+  );
 }
-
